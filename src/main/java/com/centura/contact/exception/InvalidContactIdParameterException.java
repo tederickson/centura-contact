74646aa52e98b36adc78a1d2e44330a2cfb17ac2
Ted Erickson <ted.erickson@comcast.net>
2020-06-16 11:04:44 -0600

    Update InvalidContactIdParameterException.java

--- src/main/java/com/centura/contact/exception/InvalidContactIdParameterException.java
+++ src/main/java/com/centura/contact/exception/InvalidContactIdParameterException.java
@@ -1,12 +1,15 @@
 package com.centura.contact.exception;
 
+/**
+ * @deprecated Use IllegalArgumentException instead of creating a new Exception.
+ */
+@Deprecated
 public class InvalidContactIdParameterException extends Exception {
 
 	private static final long serialVersionUID = -1117193118888293838L;
 
 	public InvalidContactIdParameterException(String message) {
 		super(message);
-
 	}
 
 }
